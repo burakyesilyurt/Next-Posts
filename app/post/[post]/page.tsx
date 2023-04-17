d4b767a79b0fe155e30aa4b1c904df21bbba9001
Burak Yesilyurt <yesilyurt.burak07@gmail.com>
2023-04-17 04:09:06 +0300

    added floatingButton

--- app/post/[post]/page.tsx
+++ app/post/[post]/page.tsx
@@ -1,5 +1,7 @@
 import { ErrorPage } from "@/components/error-page"
 import { Metadata } from 'next';
+import CardTime from "@/components/card-time"
+import { ActionButton } from "@/components/action-button"
 
 export async function generateMetadata(
   { searchParams }: QueryString,
@@ -19,21 +21,47 @@ export default function Post({ searchParams: { title, author, content, createdDa
 
 
   return (
-    <div className='grow flex min-h-screen flex-col items-center mt-24'>
-      <div className="max-w-2xl lg:w-full rounded overflow-hidden shadow-lg bg-gray-700 my-6 shadow-gray-800 w-4/5">
-        <div className="px-6 py-4">
-          <div className="flex justify-between font-bold text-xl mb-2">
-            <h1>{title}</h1>
-            <span className="mr-5">Yazar: {author}</span>
+    <>
+      <div className='grow flex flex-col items-center mt-24'>
+        <div className="max-w-2xl lg:w-full rounded overflow-hidden shadow-lg bg-gray-700 my-6 shadow-gray-800 w-4/5">
+          <div className="px-6 py-4">
+            <div className="flex justify-between font-bold text-xl mb-2">
+              <h1>{title}</h1>
+              <span className="mr-5">Yazar: {author}</span>
+            </div>
+            <p className="text-base mt-5">
+              {content}
+            </p>
           </div>
-          <p className="text-base mt-5">
-            {content}
-          </p>
-          <span>{ }</span>
-        </div>
 
+          <div className="flex justify-end mr-5 pb-5">
+            <CardTime date={createdDate} />
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div className='flex flex-col items-center'>
+        <div className="max-w-2xl lg:w-full rounded overflow-hidden shadow-lg bg-gray-700 my-6 shadow-gray-800 w-4/5 p-2">
+          <div className="px-9 py-2">
+            <div className="flex justify-between">
+              <p className="font-bold mb-2">Yazar</p>
+              <span>Saat</span>
+            </div>
+            <p className="text-base mt-5">Content</p>
+            <div id="commentTrigger" className="target:block hidden">Bingo!</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="fixed bottom-5 right-5 md:bottom-4 md:right-12 lg:bottom-20 lg:right-16">
+        <div>
+          <a href="#commentTrigger">
+            <ActionButton />
+          </a>
+        </div>
+      </div>
+    </>
+
   )
 }
 
